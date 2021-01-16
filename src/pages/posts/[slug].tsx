b03dad5fcc88a2382a57ac494cac4c8a05b76876
tharidlynn <tharidlynn@cornflakecode.com>
2021-01-17 01:11:18 +0700

    Fix missing showInfo props

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -35,6 +35,7 @@ const DisplayPost = (props: IPostProps) => (
         }}
       />
     )}
+    showInfo={false}
   >
     <h1 className="text-center font-bold text-3xl text-gray-900">{props.title}</h1>
     <div className="text-center text-sm mb-8">{format(new Date(props.date), 'LLLL d, yyyy')}</div>
